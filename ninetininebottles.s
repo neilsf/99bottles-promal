2d016258c6a57734ef9f6f0ea84fc4d8af7199c6
Csaba Fekete <feketecsaba@gmail.com>
2017-11-07 15:14:17 +0100

    Create ninetininebottles.s

---
+++ ninetininebottles.s
@@ -0,0 +1,53 @@
+program ninetyninebottles
+include library
+
+byte bottlescount
+
+proc printbottletext
+arg byte cnt
+arg byte iscaptial
+begin
+if cnt = 0
+  if iscapital = 0
+    put "no "
+  else
+    put "No "
+  put "more "
+else
+  output "#W bottle", cnt
+  if cnt > 1
+    put "s"
+put " of beer"
+end
+
+proc printoddline
+arg byte cnt
+begin
+printbottletext cnt, 1
+put "on the wall, "
+printbottletext cnt, 0
+put ".", nl
+end
+
+proc printevenline
+arg byte cnt
+begin
+put "Take one down and pass it around, "
+printbottletext cnt, 0
+put "on the wall.", nl
+end
+
+begin
+
+bottlescount = 99
+while bottlescount > 0
+  printoddline bottlescount
+  bottlescount = bottlescount-1  
+  printevenline bottlescount
+
+printoddline bottlescount
+put "Go to the store and buy some more, "
+printbottletext 99
+put "on the wall.", nl
+
+end
